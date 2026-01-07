d65a7366f6bf83b930c1920f913504e05761d5da
Jeremy Wuille <jwuille@f1br6s16.clusters.42paris.fr>
2026-01-07 14:29:49 +0100

    [fix/ex03] - Add const to AForm attributes

--- ex03/AForm.hpp
+++ ex03/AForm.hpp
@@ -9,8 +9,8 @@ class AForm
 	private:
 		const std::string	_name;
 		bool				_isSigned;
-		int					_gradeToSign;
-		int					_gradeToExecute;
+		const int					_gradeToSign;
+		const int					_gradeToExecute;
 		
 	public:
 		AForm(void);
